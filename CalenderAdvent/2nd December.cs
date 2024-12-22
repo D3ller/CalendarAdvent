b5508b93b372ae6519671df0acc1d5015521f28d
C.Nelhomme <64921289+D3ller@users.noreply.github.com>
2024-12-22 17:10:58 +0100

    2nd challenge

---
+++ CalenderAdvent/2nd December.cs
@@ -0,0 +1,20 @@
+﻿namespace MyNamespace;
+
+public class MyClass2 {
+    static void Main()
+    {
+        double number;
+
+        while (!Double.TryParse(Console.ReadLine(), out number))
+        {
+            Console.Clear();
+            Console.WriteLine("Veuillez entrer un nombre valide");
+        }
+
+        for (int i = 0; i < 11; i++)
+        {
+            Console.WriteLine($"{i} * {number} = {number * i}");
+        }
+    }
+    
+}
