ea82a4edabb123ce9a41d3a70893825b5dbb7c3e
C.Nelhomme <64921289+D3ller@users.noreply.github.com>
2024-12-22 17:38:06 +0100

    3rst challenge

---
+++ CalenderAdvent/3december.cs
@@ -0,0 +1,24 @@
+﻿namespace MyNamespace;
+
+public class ThreeDecember {
+    static void Main()
+    {
+        Console.WriteLine("Entrez un mot svp");
+        string palindrome = Console.ReadLine();
+        while (String.IsNullOrEmpty(palindrome))
+        {
+            Console.WriteLine("Veuillez entrer un mot valide");
+            palindrome = Console.ReadLine();
+        }
+
+        string reversedString = new string(palindrome.Replace(" ", "").ToLower().Reverse().ToArray());
+        Console.WriteLine(reversedString);
+        if (palindrome == reversedString)
+        {
+            Console.WriteLine("La chaîne est un palindrome");
+            return;
+        }
+        Console.WriteLine("La chaîne n'est pas un palindrome");
+    }
+    
+}
