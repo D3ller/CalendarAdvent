55b98bf7d5f065da674f61752b2f62d392de323e
C.Nelhomme <64921289+D3ller@users.noreply.github.com>
2024-12-22 17:49:17 +0100

    4 december

---
+++ CalenderAdvent/4december.cs
@@ -0,0 +1,31 @@
+﻿namespace MyNamespace;
+
+public class FourDecember {
+    static void Main()
+    {
+        Console.WriteLine("Entrez une phrase svp");
+        string phrase = Console.ReadLine();
+        while (String.IsNullOrEmpty(phrase))
+        {
+            Console.WriteLine("Veuillez entrer une phrase valide");
+            phrase = Console.ReadLine();
+        }
+
+        int vowelCount = 0;
+        string vowels = "aeiouy";
+
+        foreach (char c in phrase)
+        {
+            foreach (char v in vowels)
+            {
+                if (c == v)
+                {
+                    vowelCount++;
+                }
+            } 
+        }
+        
+        Console.WriteLine(vowelCount);
+    }
+    
+}
