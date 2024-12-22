ed001d1284c1a81818127085906ba629d0a88cb6
C.Nelhomme <64921289+D3ller@users.noreply.github.com>
2024-12-22 16:58:34 +0100

    first commit

---
+++ CalenderAdvent/1rst December.cs
@@ -0,0 +1,39 @@
+﻿namespace MyNamespace;
+
+class MyClass
+{
+    static void Main()
+    {
+        var tables = new Dictionary<int, int> {};
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            int unicode = c;
+            tables.Add(unicode, 0);
+        }
+
+        string word;
+        while (String.IsNullOrEmpty(word = Console.ReadLine()))
+        {
+            Console.Clear();
+            Console.WriteLine("Veuillez entrer des caractères entre A et Z");
+        }
+
+        word = word.ToLower().Trim().Replace(" ", "");
+
+        foreach (char l in word)
+        {
+            int unicode = l;
+            if (tables.ContainsKey(unicode))
+            {
+                tables[unicode]++;
+            }
+        }
+
+        var finalResult = from h in tables where h.Value > 0 select h;
+        foreach (var i in finalResult)
+        {
+            char letter = (char) i.Key;
+            Console.WriteLine($"{letter} : {i.Value}");
+        }
+    }
+}
