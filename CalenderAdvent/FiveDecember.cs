ac449d890d0ba2ee12cad5cd5be7d1be5d352a82
D3ller <corentin.nelhomme@gmail.com>
2024-12-23 14:01:07 +0100

    Five December

---
+++ CalenderAdvent/FiveDecember.cs
@@ -0,0 +1,21 @@
+﻿namespace MyNamespace;
+
+public class FiveDecember
+{
+    static void Main()
+    {
+        int[] dice = new int[6];
+        Random rnd = new Random();
+
+        for (int i = 0; i < 100; i++)
+        {
+            int number = rnd.Next(1, 7);
+            dice[number-1]++;
+        }
+        
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine($"Face {i+1} : {dice[i]}");
+        }
+    }
+}
