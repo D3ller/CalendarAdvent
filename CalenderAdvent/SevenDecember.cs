c342f9ddca0830924e5d3e2dc2d35a313388ca1f
D3ller <corentin.nelhomme@gmail.com>
2024-12-23 14:32:47 +0100

    Seven December

---
+++ CalenderAdvent/SevenDecember.cs
@@ -0,0 +1,32 @@
+﻿namespace MyNamespace;
+
+public class SevenDecember
+{
+
+    static void Main()
+    {
+        int result;
+        List<int> table = new List<int>();
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Veuillez rentrer un entier");
+            string number = Console.ReadLine();
+            if (number == "finish" && table.Count > 1) break;
+            
+            while (!Int32.TryParse(number, out result))
+            {
+                Console.Clear();
+                Console.WriteLine("Veuillez rentrer un entier");
+                number = Console.ReadLine();
+            }
+            
+            table.Add(result);
+        }
+    
+        int results = table.Max();
+        Console.WriteLine($"Le résultat max est {results}");
+    
+    }
+
+}
