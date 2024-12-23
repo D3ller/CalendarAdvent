a9e5000127929d44adef254e9f6e1707e8cbba43
D3ller <corentin.nelhomme@gmail.com>
2024-12-23 14:27:12 +0100

    Six December

---
+++ CalenderAdvent/SixDecember.cs
@@ -0,0 +1,30 @@
+﻿namespace MyNamespace;
+
+public class SixDecember
+{
+    static void Main()
+    {
+        int result;
+        List<int> calc = new List<int>();
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Veuillez rentrer un entier");
+            string number = Console.ReadLine();
+            if (number == "finish" && calc.Count > 1) break;
+            
+            while (!Int32.TryParse(number, out result))
+            {
+                Console.Clear();
+                Console.WriteLine("Veuillez rentrer un entier");
+                number = Console.ReadLine();
+            }
+            
+            calc.Add(result);
+        }
+
+        int results = calc.Sum();
+        Console.WriteLine($"Le résultat est {results}");
+
+    }
+}
